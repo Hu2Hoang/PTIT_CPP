acfdc5d734b999ae487e87ddb58762cd3fefdaf3
Hu2Hoang <huuhoang@pm.me>
2022-08-31 22:11:25 +0700

    200 - 31/8

---
+++ CPP0727.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Code designed by https://linktr.ee/hoangdinh314
+#define ll long long
+#define pb push_back
+#define fi first
+#define se second
+#define sz(a) int((a).size())
+#define ms(s,n) memset(s,n,sizeof(s))
+#define fio(i,a1,b1) for(int i=a1;i<b1;i++)
+#define w(t) int t;scanf("%d",&t);while(t--)
+#define tolowerr(str) transform(str.begin(),str.end(),str.begin(),::tolower);
+#define toupperr(str) transform(str.begin(),str.end(),str.begin(),::toupper);
+#define imps(vs) vector<string> vs; fio(i,0,n){ string x;cin>>x; vs.push_back(x); }
+#define exps(vs) for(auto x:vs) cout<<x<<' ';
+#define imp(vi) vector<int> vi; fio(i,0,n){int x;cin>>x;vi.push_back(x);}
+#define exp(vi) for(int x:vi) cout<<x<<' ';
+#define sovi(vi) sort(vi.begin(),vi.end())
+#define mcd(n) vector<int> mcd(n); partial_sum(vi.begin(),vi.end(),mcd.begin());//mcd[r-1]-mcd[l-2]
+#define mav(vi) *max_element(vi.begin(),vi.end())
+#define acm(vi) accumulate(vi.begin(),vi.end(),0)
+inline ll lcm(ll a,ll b){return (a*b)/__gcd(a,b);}
+const int mod=1e9+7;
+int main(){
+	w(t){
+		int n;cin>>n;
+		int a[n];
+		for(int &x:a) cin>>x;
+		if(n==1){cout<<a[0];return 0;}
+		if(n==2){cout<<max(a[0],a[1]);}
+		else{
+			ll b=a[0],c=a[1],d=a[0]+a[2];
+			fio(i,3,n){
+				ll e=a[i];
+				if(b>c) e+=b;
+				else e+=c;
+				b=c;c=d,d=e;
+			}
+			cout<<max(c,d);
+		}
+		cout<<endl;
+	}
+	return 0;
+}
