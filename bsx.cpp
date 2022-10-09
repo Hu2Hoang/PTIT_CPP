c1c0de127f83d522fa7d090038f54aff2f93e00e
Hoang Dinh <91410379+Hu2Hoang@users.noreply.github.com>
2022-10-09 14:37:39 +0700

    Add files via upload

---
+++ bsx.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Code designed by https://linktr.ee/hoangdinh314
+#define ll long long
+#define pb push_back
+#define fi first
+#define se second
+#define sz(a) int((a).size())
+#define ms(s,n) memset(s,n,sizeof(s))
+#define fio(i,a1,b1) for(int i=a1;i<b1;i++)
+#define w(t) int t;scanf("%d",&t);while(t--)
+#define tolowerr(str) transform(str.begin(),str.end(),str.begin(),::tolower);
+#define toupperr(str) transform(str.begin(),str.end(),str.begin(),::toupper);
+#define imps(vs) vector<string> vs; fio(i,0,n){ string x;cin>>x; vs.push_back(x); }
+#define exps(vs) for(auto x:vs) cout<<x<<' ';
+#define imp(vi) vector<int> vi; fio(i,0,n){int x;cin>>x;vi.push_back(x);}
+#define exp(vi) for(int x:vi) cout<<x<<' ';
+#define sovi(vi) sort(vi.begin(),vi.end())
+#define mcd(n) vector<int> mcd(n); partial_sum(vi.begin(),vi.end(),mcd.begin());//mcd[r-1]-mcd[l-2]
+#define mav(vi) *max_element(vi.begin(),vi.end())
+#define acm(vi) accumulate(vi.begin(),vi.end(),0)
+inline ll lcm(ll a,ll b){return (a*b)/__gcd(a,b);}
+const int mod=1e9+7;
+int main(){
+	int n;cin>>n;
+	cin.ignore();
+	int res=0;
+	map<string,long long> mp;
+	fio(i,0,n){
+		string bien,loai,io,time;
+		int sl;
+		cin>>bien>>loai>>sl>>io>>time;
+			if(io=="OUT") continue;
+			else{
+				if(loai=="Xe_con"){
+					switch (sl){
+						case 5:{
+							mp[time]+=10000;
+							break;
+						}
+						case 7:{
+							mp[time]+=15000;
+							break;
+						}
+					}
+				}
+				else if(loai=="Xe_tai"){
+					mp[time]+=20000;
+				}
+				else if(loai=="Xe_khach"){
+					switch(sl){
+						case 29:{
+							mp[time]+=50000;
+							break;
+						}
+						case 45:{
+							mp[time]+=70000;
+							break;
+						}
+					}
+				}
+			}
+	}
+	for(auto x:mp){
+		cout<<x.first<<": "<<x.second<<endl;
+	}
+	return 0;
+}
